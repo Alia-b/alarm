8a1d011ddf934f66427c338ac13769bc38f3a8d7
Sebastian- <b.c.sebastian@gmail.com>
2012-06-21 15:26:08 -0700

    added argparser, removed wxwidget

--- alarm.py
+++ alarm.py
@@ -1,11 +1,40 @@
 #!/usr/bin/python2.7
 
-import os,wx 
+import os,argparse 
 from mplayer import Player, CmdPrefix
 
+
+path_to_script = "/home/bielobog/code/python/alarm/alarm.py"
+
+#Top level parser
+parser = argparse.ArgumentParser(description="Set and manage an alarm")
+subparsers = parser.add_subparser()
+
+#Time parser
+time_parser = subparsers.add_parser("set",help = "Set the alarm")
+time_parser.add_argument('time',help="The time to play the alarm")
+
+#Play parser
+play_parser = subparser.add_parser("play",help="Play alarm")
+
+#Stop parser
+stop_parser = subparser.add_parser("stop",help="Stop the alarm")
+
+args = parser.parse_args()
+
+
+def set_alarm(script_path,time):
+    os.system("echo \" "+script_path+" \" |  at "+time)
+
+if args.time:
+    set_alarm(path_to_script,args.time)
+
+def snooze(time,player):
+    player.pause()
+    wait(time)
+    player.play()
+
 music = '/home/bielobog/Music/Singles'
-
-
 
 def get_songs(music_dir):
     """
@@ -19,21 +48,18 @@ def get_songs(music_dir):
             songs.append(os.path.join(entry[0],song))
     return songs
 
-def start_playback(song):
+def start_playback(songs,random=True):
     Player.cmd_prefix = CmdPrefix.PAUSING_KEEP
     player = Player()
-    player.loadfile(song)
+    player.loadfile(song[0])
 
     return player
 
-songs = get_songs(music)
-foo = start_playback(songs[1])
+def run():
+    songs = get_songs(music)
+    player = start_playback(songs)
 
-app = wx.App()
-frame = wx.Frame(None,-1,"Alarm")
-frame.Show()
-app.MainLoop()
-
-
+def quit(player):
+    player.quit()
 
 
